c21efeeea108f2f97aeb6aa93155bf77fe1c8d98
Wayne Osborn <jacobwayne1513@aol.com>
2023-09-15 12:41:06 -0400

    league id should be optional

--- read/src/main.ts
+++ read/src/main.ts
@@ -10,18 +10,18 @@ dotenv.config({ path: '../.env' })
 const main = async () => {
 
     const envWeek = process.env.WEEK
-    if(!envWeek) throw new Error('No WEEK provided, please include a .env file with WEEK')
+    if (!envWeek) throw new Error('No WEEK provided, please include a .env file with WEEK')
     const WEEK = parseInt(envWeek)
     const envYear = process.env.YEAR
-    if(!envYear) throw new Error('No YEAR provided, please include a .env file with YEAR')
+    if (!envYear) throw new Error('No YEAR provided, please include a .env file with YEAR')
     const YEAR = parseInt(envYear)
     const LEAGUE_ID = process.env.SLEEPER_LEAGUE_ID
-    if(!LEAGUE_ID) throw new Error('No YEAR provided, please include a .env file with YEAR')
+    if (!LEAGUE_ID) console.warn('No SLEEPER_LEAGUE_ID provided, please include a .env file with SLEEPER_LEAGUE_ID if you want to use the roster feature')
 
     await loadPlayers()
     await loadSeasonStats(YEAR)
     await loadWeeklyStats(WEEK, YEAR)
-    await loadRosters(LEAGUE_ID)
+    if (LEAGUE_ID) await loadRosters(LEAGUE_ID)
     await calculateData(WEEK)
 }
 
