dd5b404fd300d415b818961b8a84691e73078be9
Wayne Osborn <jacobwayne1513@aol.com>
2023-09-25 09:28:19 -0400

    start ts conversion for ui

--- view/server/getIndex.ts
+++ view/server/getIndex.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { SearchPosition } from '../../shared/types'
 import { TopPlayerReturn } from '../../shared/api-types'
 
@@ -10,5 +11,18 @@ export const topPlayers = async (position: SearchPosition) => {
 export const fiveWeekTopPlayers = async (position: SearchPosition)=> {
   const res = await fetch(`http://localhost:3001/fiveWeeks/${position}`)
   const data: TopPlayerReturn = await res.json()
+=======
+import { EligiblePositions } from '../../shared/types'
+
+export const top50 = async (position: EligiblePositions) => {
+  const res = await fetch(`http://localhost:3001/allSeason/${position}`)
+  const data = await res.json()
+  return data
+}
+
+export const fiveWeekTop50 = async (position: EligiblePositions)=> {
+  const res = await fetch(`http://localhost:3001/fiveWeeks/${position}`)
+  const data = await res.json()
+>>>>>>> 8a50390 (start ts conversion for ui)
   return data
 }
